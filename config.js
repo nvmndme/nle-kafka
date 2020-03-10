0c81c65a111db649dee5d84235edcc2112b76038
nvmndme <caesargilang@gmail.com>
2020-03-11 05:31:01 +0700

    invisible things

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // url: 'mongodb://10.241.145.145:27017/officedjbc',
-    // kafka_host: 'kafkadrc01.customs.go.id:9092,kafkadrc02.customs.go.id:9092,kafkadrc03.customs.go.id:9092',
-    kafka_host: 'localhost:9092',
+    kafka_host: 'kafkadrc01.customs.go.id:9092,kafkadrc02.customs.go.id:9092,kafkadrc03.customs.go.id:9092',
+    // kafka_host: 'localhost:9092',
     port: 3000,
     mongourl: 'mongodb://nle1:nle1@10.161.3.9:27018/nle',
     dbname: 'nle'
