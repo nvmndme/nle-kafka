4434e6e74a3059af309c9bdb61de3e7ebf82f96c
caesar.gilang <caesar.gilang@kemenkeu.go.id>
2020-03-10 13:04:24 +0700

    no filter

--- route.js
+++ route.js
@@ -2,19 +2,19 @@ module.exports = (app) => {
     const contr = require('./controller.js');
 
     // [PLATFORM] Retrieve all available Bookings
-    app.get('/Booking', contr.bookings);
+    app.get('/api/bookings', contr.bookings);
 
     // [PLATFORM] [PENGGUNA JASA] Retrieve Booking detail with user id uid
-    app.get('/DetilBooking/idUser/:uid', contr.bookingUid);
+    app.get('/api/bookings/:uid', contr.bookingUid);
 
     // [PENGGUNA JASA] Post Booking
-    app.post('/Booking', contr.sendBooking);
+    app.post('/bookings', contr.sendBooking);
 
     // [PLATFORM] [PENGGUNA JASA] Retrieve all available Offers with Booking Number bn
     app.get('/offers/:bn', contr.offersBn);
 
     // [PLATFORM] [PENGGUNA JASA] Retrieve Offer detail with Offer number on
-    app.get('/offers/:bn', contr.offerOn);
+    app.get('/offers/:on', contr.offerOn);
 
     // [PLATFORM] Post Offers for Booking number bn
     app.post('/offers/', contr.sendOffers);
