ec5401cba57370cb17ac196b656490a34b72a13d
caesar.gilang <caesar.gilang@kemenkeu.go.id>
2020-03-12 12:08:00 +0700

    never alone, but always depressed

--- route.js
+++ route.js
@@ -1,11 +1,8 @@
 module.exports = (app) => {
     const contr = require('./controller.js');
 
-    // [PLATFORM] Retrieve all available Bookings from Platform :id
+    // [PLATFORM] Retrieve all available Bookings
     app.get('/api/bookings', contr.bookings);
-
-    // [PLATFORM] [PENGGUNA JASA] Retrieve Booking detail with user id uid
-    app.get('/api/bookings/:uid', contr.bookingUid);
 
     // [PENGGUNA JASA] Post Booking
     app.post('/api/bookings', contr.sendBooking);
@@ -13,9 +10,15 @@ module.exports = (app) => {
     // [PLATFORM] Post Offers for Booking number bn
     app.post('/api/offers', contr.sendOffers);
 
-    // [PENGGUNA JASA] Post Selected Offer
-    app.post('/api/selectOffer', contr.sendSelectedOffer);
+    // [PENGGUNA JASA] Post checkout Offer
+    app.post('/api/checkouts', contr.sendCheckout);
 
-    // [PLATFORM] Retrieve Selected Offer
-    app.get('/api/selectOffer', contr.sendSelectedOffer);
+    // [PLATFORM] Retrieve checkout Offer
+    app.get('/api/checkouts', contr.checkout);
+
+    // [PLATFORM] Post payment
+    app.post('/api/payments', contr.sendPayment);
+
+    // [PLATFORM] Retrieve payment
+    app.get('/api/payments', contr.payment);
 };
