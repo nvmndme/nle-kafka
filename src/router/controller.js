e643d5cf72e9f4cbe4cdb925bdf14f3e19bc987f
caesar.gilang <caesar.gilang@kemenkeu.go.id>
2020-03-19 12:13:26 +0700

    we

--- src/router/controller.js
+++ src/router/controller.js
@@ -6,6 +6,61 @@ const validator = require('../modules/validator');
 var Consumer = kafka.Consumer;
 const Producer = kafka.HighLevelProducer;
 
+//NLE -> KAFKA
+exports.sendBooking = (req, res) => {
+    const book = req.body;
+    const kafka_topic = config.bookingTopic;
+    const client = new kafka.KafkaClient({
+        kafkaHost: config.kafka_host
+    });
+    const producer = new Producer(client);
+    
+    producer.on('ready', function () {
+        var message = book;
+        
+        var payload = [{
+            topic: kafka_topic,
+            messages: JSON.stringify(message),
+            key: book.idRequestBooking,
+            attributes: 1 /* Use GZip compression for the payload */
+        }];
+        
+        producer.send(payload, function (error, result) {
+            console.info('Sent payload to Kafka: ', payload);
+            if (error) {
+                console.error('error: ', error);
+                res.status(403).end();
+                process.exit();
+            } else {
+                console.log('result: ', result);
+                res.status(201).end();
+                process.exit();
+            }
+        });
+    });
+    
+    producer.on('error', function (err) {
+        console.log('error', err);
+        res.status(408).end();
+        process.exit();
+    });
+    
+    process.on('SIGINT', function () {
+        producer.close(true, function () {
+            res.status(400).end();
+            process.exit();
+        });
+    });
+    
+    process.on('SIGHUP', function () {
+        producer.close(true, function () {
+            res.status(400).end();
+            process.exit();
+        });
+    });
+};
+
+//KAFKA -> PLATFORM
 exports.booking = (req, res) => {
     groupId = req.get('Platform-Id');
     validator.validateHeader(groupId);
@@ -63,59 +118,7 @@ exports.booking = (req, res) => {
     });
 };
 
-exports.sendBooking = (req, res) => {
-    const book = req.body;
-    const kafka_topic = config.bookingTopic;
-    const client = new kafka.KafkaClient({
-        kafkaHost: config.kafka_host
-    });
-    const producer = new Producer(client);
-    
-    producer.on('ready', function () {
-        var message = book;
-        
-        var payload = [{
-            topic: kafka_topic,
-            messages: JSON.stringify(message),
-            key: book.id_platform,
-            attributes: 1 /* Use GZip compression for the payload */
-        }];
-        
-        producer.send(payload, function (error, result) {
-            console.info('Sent payload to Kafka: ', payload);
-            if (error) {
-                console.error('error: ', error);
-                res.status(403).end();
-                process.exit();
-            } else {
-                console.log('result: ', result);
-                res.status(201).end();
-                process.exit();
-            }
-        });
-    });
-    
-    producer.on('error', function (err) {
-        console.log('error', err);
-        res.status(408).end();
-        process.exit();
-    });
-    
-    process.on('SIGINT', function () {
-        producer.close(true, function () {
-            res.status(400).end();
-            process.exit();
-        });
-    });
-    
-    process.on('SIGHUP', function () {
-        producer.close(true, function () {
-            res.status(400).end();
-            process.exit();
-        });
-    });
-};
-
+//PLATFORM -> KAFKA
 exports.sendOffer = (req, res) => {
     const offer = req.body;
     const kafka_topic = config.offerTopic;
@@ -130,7 +133,7 @@ exports.sendOffer = (req, res) => {
         var payload = [{
             topic: kafka_topic,
             messages: JSON.stringify(message),
-            key: offer.idPlatform.toString() + '-' + offer.idRequestBooking.toString(),
+            key: offer.idRequestBooking.toString(),
             attributes: 1 /* Use GZip compression for the payload */
         }];
         
@@ -166,7 +169,11 @@ exports.sendOffer = (req, res) => {
     });
 };
 
+//NLE -> KAFKA
 exports.sendCheckout = (req, res) => {
+    //API-04
+    //idRequestBooking, idServiceOrder, idPlatform, payment_method, payment_channel
+
     const checkout = req.body;
     const kafka_topic = config.checkoutTopic;
     const client = new kafka.KafkaClient({
@@ -180,7 +187,7 @@ exports.sendCheckout = (req, res) => {
         var payload = [{
             topic: kafka_topic,
             messages: JSON.stringify(message),
-            key: checkout.idRequestBooking.toString() + '-' + checkout.idOffer.toString(),
+            key: checkout.idRequestBooking.toString(),
             attributes: 1 /* Use GZip compression for the payload */
         }];
         
@@ -218,6 +225,7 @@ exports.sendCheckout = (req, res) => {
     });
 };
 
+//KAFKA -> PLATFORM
 exports.checkout = (req, res) => {
     groupId = req.get('Platform-Id');
     validator.validateHeader(groupId);
@@ -278,6 +286,7 @@ exports.checkout = (req, res) => {
     
 };
 
+//PLATFORM -> KAFKA
 exports.sendPayment = (req, res) => {
     const payment = req.body;
     const kafka_topic = config.paymentTopic;
@@ -292,7 +301,7 @@ exports.sendPayment = (req, res) => {
         var payload = [{
             topic: kafka_topic,
             messages: JSON.stringify(message),
-            key: payment.idRequestBooking.toString() + '-' + payment.idOffer.toString(),
+            key: payment.idRequestBooking.toString(),
             attributes: 1 /* Use GZip compression for the payload */
         }];
         
@@ -329,22 +338,22 @@ exports.sendPayment = (req, res) => {
     });
 };
 
-//belum diedit
+//PLATFORM -> KAFKA
 exports.sendTruckDriver = (req, res) => {
-    const payment = req.body;
-    const kafka_topic = config.paymentTopic;
+    const truckDriver = req.body;
+    const kafka_topic = config.truckTopic;
     const client = new kafka.KafkaClient({
         kafkaHost: config.kafka_host
     });
     const producer = new Producer(client);
     
     producer.on('ready', function () {
-        var message = payment;
+        var message = truckDriver;
         
         var payload = [{
             topic: kafka_topic,
             messages: JSON.stringify(message),
-            key: payment.idRequestBooking.toString() + '-' + payment.idOffer.toString(),
+            key: payment.idRequestBooking.toString(),
             attributes: 1 /* Use GZip compression for the payload */
         }];
         
@@ -381,22 +390,22 @@ exports.sendTruckDriver = (req, res) => {
     });
 };
 
-//belum diedit
+//PLATFORM -> KAFKA
 exports.sendBookingStatus = (req, res) => {
-    const payment = req.body;
-    const kafka_topic = config.paymentTopic;
+    const bookingStatus = req.body;
+    const kafka_topic = config.bookingStatusTopic;
     const client = new kafka.KafkaClient({
         kafkaHost: config.kafka_host
     });
     const producer = new Producer(client);
     
     producer.on('ready', function () {
-        var message = payment;
+        var message = bookingStatus;
         
         var payload = [{
             topic: kafka_topic,
             messages: JSON.stringify(message),
-            key: payment.idRequestBooking.toString() + '-' + payment.idOffer.toString(),
+            key: payment.idRequestBooking.toString(),
             attributes: 1 /* Use GZip compression for the payload */
         }];
         
@@ -433,22 +442,22 @@ exports.sendBookingStatus = (req, res) => {
     });
 };
 
-//belum diedit
+//PLATFORM -> KAFKA
 exports.sendTruckStatus = (req, res) => {
-    const payment = req.body;
-    const kafka_topic = config.paymentTopic;
+    const truckStatus = req.body;
+    const kafka_topic = config.truckStatusTopic;
     const client = new kafka.KafkaClient({
         kafkaHost: config.kafka_host
     });
     const producer = new Producer(client);
     
     producer.on('ready', function () {
-        var message = payment;
+        var message = truckStatus;
         
         var payload = [{
             topic: kafka_topic,
             messages: JSON.stringify(message),
-            key: payment.idRequestBooking.toString() + '-' + payment.idOffer.toString(),
+            key: payment.idRequestBooking.toString(),
             attributes: 1 /* Use GZip compression for the payload */
         }];
         
